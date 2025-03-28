56acbef160f74cb79d4b87b2d455dbd7938689f9
50/50 <80717571+50n50@users.noreply.github.com>
2025-03-28 15:48:01 +0400

    Create dev.js

---
+++ dev/dev.js
@@ -0,0 +1,15 @@
+async function searchResults(keyword){
+  
+}
+
+async function extractDetails(url){
+  
+}
+
+async function extractEpisodes(url){
+  
+}
+
+async function extractStreamUrl(url){
+  
+}
